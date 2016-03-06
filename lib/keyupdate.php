28e4a9bf52a59c7af53ad48161ce76fa65984434
Joel <Joel.bethke@gmail.com>
2016-03-05 19:51:22 -0600

    Code cleanup

--- lib/keyupdate.php
+++ lib/keyupdate.php
@@ -1,7 +1,7 @@
 <?php
 
 function updateStreamkey($input, $function) {
-	require('/var/www/html/lib/dbconnect.php');
+	require '/var/www/html/lib/dbconnect.php';
 	if ($function === 'channel') {
 		$kuresults = pg_fetch_assoc(pg_query($pglink, "SELECT channel_name FROM users WHERE stream_key = '$input'"));
 		$kuchannelname = $kuresults['channel_name'];
