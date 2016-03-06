e759bd73b77dc06b3ea6132fdf3fe00629adb495
Joel <Joel.bethke@gmail.com>
2016-03-05 19:49:24 -0600

    Authentication functions. WIP (works with inc/auth.php)

--- lib/auth.php
+++ lib/auth.php
@@ -1,42 +1,38 @@
 <?php
 
-session_start();
-require('/var/www/html//lib/dbconnect.php');
-$auth_email = filter_var($_COOKIE['email'], FILTER_VALIDATE_EMAIL);
-$auth_result = pg_query($pglink, "SELECT * FROM users WHERE email = '$auth_email'");
-$auth_row_cnt = pg_num_rows($auth_result);
-//echo $auth_row_cnt;
-//if ($auth_row_cnt != 1) {
-//    session_destroy();
-//    setcookie('rememberMe', null, -1, '/');
-//    setcookie('email', null, -1, '/');
-//    header('Location: login.php');
-//    echo 'destroyed';
-//} else {
-	//echo 'not destroyed';
-//}
-if (empty($_SESSION['authenticated']) && $_COOKIE['rememberMe'] === true && !empty($_COOKIE['email'])) {
-    $_SESSION['authenticated'] = $_COOKIE['email'];
-}
-if (!empty($_SESSION['authenticated'])) {
-    //$email = filter_var($_SESSION['authenticated'], FILTER_VALIDATE_EMAIL);
-    //$check = pg_fetch_assoc(pg_query($pglink, "SELECT * FROM users WHERE email = '$email'"));
-    //$channel_name = $check['channel_name'];
-    //$channel_title = $check['channel_title'];
-    if (isset($_GET["action"])) {
-	$action = $_GET['action'];
-	if ($action == 'logout') {
-	    session_destroy();
-	    setcookie('rememberMe', null, -1, '/');
-	    setcookie('email', null, -1, '/');
-	    header('Location: login.php');
-	}
-	if ($action == 'already_logged_in') {
-	    $status = 'Already logged in!';
-	}
-    }
-} else {
-    header('Location: login.php');
-}
+/*	**************	*
+ * 	/lib/auth.php	*
+ * 	**************	*/
 
+class auth extends database {
 
+	// check if the cookie is valid. This prevents deleted users with a cookie from being able to log in the site.
+	public function cookiecheck($email) {
+		if ($email) {
+			$params = array($email);
+			$sql = "SELECT * FROM users WHERE email = $1";
+			$result = pg_query_params($this->link, $sql, $params);
+			$row_cnt = pg_num_rows($result);
+			if ($row_cnt < 1) {
+				session_destroy();
+				setcookie('rememberMe', null, -1, '/');
+				setcookie('email', null, -1, '/');
+				header("Location: login.php");
+			}
+		}
+	}
+
+	public function session_authenticate($email, $action = null) {
+
+		if ($action == 'logout') {
+			session_destroy();
+			setcookie('rememberMe', null, -1, '/');
+			setcookie('email', null, -1, '/');
+			header('Location: login.php');
+		} else {
+			echo 'All good?';
+			//header('Location: login.php');
+		}
+	}
+
+}
