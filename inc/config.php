796e5d273626a8cf2979ecb6e29cac3ea087ad01
Joel <Joel.bethke@gmail.com>
2016-03-08 15:23:30 -0600

    Changed $surl and $furl to by generated dynamically

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,24 @@
 <?php
 
-/*    ****************    *
- *    Site config vars    *
- *    ****************    */
+/*
+ *---------------------------------------------------------------
+ * Global site configuaration
+ *---------------------------------------------------------------
+ *
+ * This page contains global site config variables. Right now
+ * it just has the debug functions var amd site URL strings, but 
+ * I will likely update this later to include more things as I 
+ * find uses for it. This should be included on any user-facing pages.
+ * 
+ * TODO:
+ *
+ *     -Think of ways to better use this
+ *
+ */
 
-$debug = true;
+$debug = true; // setting to true will enable debug output in various places. Live sites should set to false.
+$protocol = $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$surl = $_SERVER['HTTP_HOST'];
+$furl = $protocol . $surl;
 
 
