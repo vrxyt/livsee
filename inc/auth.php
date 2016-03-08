7e17bc07e13a32b18abfc4a6695453da0da7d52e
Joel <Joel.bethke@gmail.com>
2016-03-08 15:22:50 -0600

    Moved most auth to function, added description

--- inc/auth.php
+++ inc/auth.php
@@ -1,23 +1,38 @@
 <?php
 
-// Include this file on pages you want hidden behind the auth system
+/*
+ *---------------------------------------------------------------
+ * Authentication check script
+ *---------------------------------------------------------------
+ *
+ * This page is used for verifying that a user is actually logged in.
+ * It will check for cookie, set that as active session if session isn't
+ * already present. It will also verify a cookie is valid against the
+ * user database, to prevent a deleted user from connecting with a 
+ * previously valid cookie.
+ * 
+ * TODO:
+ *
+ *     -Make this whole page not suck
+ *
+ * NOTE: This file sets up both the $user for use later
+ *		 If this file is included, do not set again on the page.
+ */
 
 session_start();
 require_once 'inc/functions.php';
 $user = new user();
-$auth = new auth();
 $action = null;
-if (!empty($_COOKIE['email'])) { $auth->cookiecheck($_COOKIE['email']); }
+
+if (!empty($_COOKIE['email']) && $user->session_check($_COOKIE['email']) === false) {
+    session_destroy();
+    setcookie('rememberMe', null, -1, '/');
+    setcookie('email', null, -1, '/');
+    header("Location: login.php");
+}
+
 if (isset($_GET["action"])) { $action = $_GET['action']; }
 
-if (empty($_SESSION['authenticated']) && $_COOKIE['rememberMe'] === true && !empty($_COOKIE['email'])) {
-	$_SESSION['authenticated'] = $_COOKIE['email'];
-}
-
-if (!empty($_SESSION['authenticated'])) {
-	$email = $_SESSION['authenticated'];
-	$check_auth = $auth->session_authenticate($email, $action);
-}
-else { header('Location: login.php'); }
+if (!empty($_SESSION['authenticated']) && $user->session_authenticate($_SESSION['authenticated'], $action) === false) {	header('Location: login.php'); }
 
 
