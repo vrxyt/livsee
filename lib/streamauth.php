bd92e0843803439c6df77668cf3d9d16379f8088
Joel <Joel.bethke@gmail.com>
2016-03-23 10:48:30 -0500

    Updated to new RTMP class functions and global site configs.

--- lib/streamauth.php
+++ lib/streamauth.php
@@ -1,36 +1,48 @@
 <?php
 
-$file = '/var/log/nginx/streamauth.log';
+/*
+ * ---------------------------------------------------------------
+ * Stream authentication
+ * ---------------------------------------------------------------
+ *
+ * This file must be specified in the nginx RTMP block using the
+ * on_publish directive. Example:
+ * 
+ *		# Live Stream Application
+ *		application live {
+ *			live on;
+ *			on_publish http://path/to/streamauth.php;
+ *		}
+ *
+ */
+
+require_once '../lib/database.php';
+require_once '../inc/config.php';
+require_once 'rtmp.php';
+
+$rtmp = new rtmp();
+$key = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+
 $timestamp = new DateTime();
 $timestamp = $timestamp->format('Y-m-d H:i:s');
 $timestamp = "Connection Attempt: $timestamp\r\n";
+
 $getstring = $timestamp;
 $getstring .= print_r($_GET, true);
-file_put_contents($file, $getstring, FILE_APPEND | LOCK_EX);
-echo var_dump($_GET);
+
+file_put_contents($SAlogfile, $getstring, FILE_APPEND | LOCK_EX);
 //check if querystrings exist or not
-if (empty($_GET['name'])) {
+if (empty($key)) {
 	//no querystrings or wrong syntax
 	$current = "wrong query input\n";
-	file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
+	file_put_contents($SAlogfile, $current, FILE_APPEND | LOCK_EX);
 	echo "wrong query input";
-	header('HTTP/1.0 404 Not Found');
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 	exit(1);
 } else {
 	//check and verify key against the DB
-	require '/var/www/html/lib/dbconnect.php';
-	$key = $_GET['name'];
-	$result = pg_query($pglink, "SELECT * FROM users WHERE stream_key = '$key'");
-	$row_cnt = pg_num_rows($result);
-	if ($row_cnt >= 1) {
-		$current = "Result: Valid stream key!\r\n";
-		file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
-		echo "Valid stream key!";
-		return $_GET;
-	} else {
-		$current = "Result: Invalid stream key!\r\n";
-		file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
-		echo "Invalid stream key!";
-		header('HTTP/1.0 404 Not Found');
+	$check = $rtmp->stream_check($key, $SAlogfile);
+	if ($check === false) {
+		header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 	}
 }
