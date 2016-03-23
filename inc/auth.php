b7f44bcb7318dce6b9fe32ce89dcf04dde779052
Joel <Joel.bethke@gmail.com>
2016-03-23 10:08:53 -0500

    Fixed some logic, made this actually work.

--- inc/auth.php
+++ inc/auth.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- *---------------------------------------------------------------
+ * ---------------------------------------------------------------
  * Authentication check script
- *---------------------------------------------------------------
+ * ---------------------------------------------------------------
  *
  * This page is used for verifying that a user is actually logged in.
  * It will check for cookie, set that as active session if session isn't
@@ -15,8 +15,8 @@
  *
  *     -Make this whole page not suck
  *
- * NOTE: This file sets up both the $user for use later
- *		 If this file is included, do not set again on the page.
+ * NOTE: This file sets up $user for use later
+ * 		 If this file is included, do not set again on the page.
  */
 
 session_start();
@@ -25,14 +25,21 @@ $user = new user();
 $action = null;
 
 if (!empty($_COOKIE['email']) && $user->session_check($_COOKIE['email']) === false) {
-    session_destroy();
-    setcookie('rememberMe', null, -1, '/');
-    setcookie('email', null, -1, '/');
-    header("Location: login.php");
+	session_destroy();
+	setcookie('rememberMe', null, -1, '/');
+	setcookie('email', null, -1, '/');
+	header("Location: login.php");
 }
 
-if (isset($_GET["action"])) { $action = $_GET['action']; }
+if (isset($_GET["action"])) {
+	$action = $_GET['action'];
+}
 
-if (!empty($_SESSION['authenticated']) && $user->session_authenticate($_SESSION['authenticated'], $action) === false) {	header('Location: login.php'); }
+if (!empty($_SESSION['authenticated']) && $user->session_authenticate($_SESSION['authenticated'], $action) === false) {
+	header('Location: login.php');
+}
 
+if (empty($_SESSION['authenticated'])) {
+	header('Location: login.php');
+}
 
