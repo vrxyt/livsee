1e459df8cd3c1c2e29f1c35973fdc729cb6bbe92
Joel <Joel.bethke@gmail.com>
2016-04-28 00:08:36 -0500

    Massive update to v2.0.

---
+++ lib/database.class.php
@@ -0,0 +1,36 @@
+<?php
+/*    ******************   *
+ *    /lib/database.class.php    *
+ *    ******************   */
+
+
+/* NOTE: This site uses PostgreSQL. If you want to use MySQL you will need to change all the DB queries. */
+
+class database {
+
+// Connection params
+	private $host = 'localhost';
+	private $port = '5432';
+	private $user = '';
+	private $password = '';
+	private $dbname = '';
+// This can be accessed by the database class and any class that extends database by using $this->link
+	public $link;
+
+	public function __construct() {
+
+// Open database connection
+		$this->link = pg_connect("host=$this->host port=$this->port dbname=$this->dbname user=$this->user password=$this->password");
+		if (!$this->link) {
+			$message = "A connection error occured. ";
+			$message .= pg_connection_status($this->link);
+			throw new Exception($message);
+		}
+	}
+
+	public function __destruct() {
+		// Close database connection -- NOTE: Apparently not needed?
+		//if ($this->link) { pg_close($this->link); }
+	}
+
+}
