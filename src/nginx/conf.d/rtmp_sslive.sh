eb8c986d8671c9342b301e2248e27b09fc76c2df
Joel <Joel.bethke@gmail.com>
2016-05-19 10:57:20 -0500

    Added nginx config files in prep for release

---
+++ src/nginx/conf.d/rtmp_sslive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+on_die () {
+    pkill -KILL -P $$
+}
+
+trap 'on_die' TERM echo $(date +[%FT%TZ]) screenshot screenshot_$1.png >> /var/log/nginx/screenshot.log
+
+ffmpeg -i rtmp://localhost/live/$1 -f image2 -vframes 1 -y /var/www/html/img/channel/channel_$1.png
+ffmpeg -i rtmp://localhost/live/$1 -f image2 -vframes 1 -s 540x304 -y /var/www/html/img/thumbs/thumb_$1.png
+
+sleep 15s
