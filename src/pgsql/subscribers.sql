3f62d731e0e75c0491c7478e09c8f4b838bc40d1
Joel <Joel.bethke@gmail.com>
2016-05-19 17:20:42 -0500

    SQL table create scripts

---
+++ src/pgsql/subscribers.sql
@@ -0,0 +1,20 @@
+﻿-- Table: subscribers
+
+-- DROP TABLE subscribers;
+
+CREATE TABLE subscribers
+(
+  host_account text NOT NULL,
+  subscriber text NOT NULL,
+  CONSTRAINT subscribers_pk PRIMARY KEY (host_account, subscriber),
+  CONSTRAINT host_account_fk FOREIGN KEY (host_account)
+      REFERENCES users (email) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE subscribers
+  OWNER TO pg_sql_account_here;
+COMMENT ON TABLE subscribers
+  IS 'Table for managing subscribers for channels';
