90e0cffe78d52da1e946293b809a99336532a25a
Joel <Joel.bethke@gmail.com>
2016-05-19 17:17:17 -0500

    SQL table create scripts

---
+++ src/pgsql/users.sql
@@ -0,0 +1,25 @@
+﻿-- Table: users
+
+-- DROP TABLE users;
+
+CREATE TABLE users
+(
+  email text NOT NULL,
+  password text,
+  auth_code integer,
+  verified integer,
+  channel_name text,
+  channel_title text,
+  stream_key character(10),
+  display_name text,
+  profile_img text,
+  api_key text,
+  CONSTRAINT plk_email PRIMARY KEY (email)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE users
+  OWNER TO pg_sql_account_here;
+COMMENT ON TABLE users
+  IS 'Table for managing user information/data';
