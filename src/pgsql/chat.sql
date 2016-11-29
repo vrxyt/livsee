765efeee110c698f2ba9b4ef78387b46fced7aea
Joel Bethke <joel.bethke@gmail.com>
2016-11-29 17:27:35 -0600

    Add chat database creation file

---
+++ src/pgsql/chat.sql
@@ -0,0 +1,21 @@
+﻿-- Table: chat
+
+-- DROP TABLE chat;
+
+CREATE TABLE chat
+(
+  id serial NOT NULL,
+  channel_email text,
+  sender text,
+  message text,
+  "timestamp" bigint,
+  type text, -- Valid params are USER or SYSTEM currently
+  CONSTRAINT pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE chat
+  OWNER TO changeme;
+COMMENT ON COLUMN chat.type IS 'Valid params are USER or SYSTEM currently';
+
