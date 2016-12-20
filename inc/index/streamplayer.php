1c8feed8e998659cc96cd3fdd8f6c0b0fc21380b
Joel Bethke <joel.bethke@gmail.com>
2016-12-19 23:21:37 -0600

    Clean up stream player and make compatible with new video.js functions. Add chat var

--- inc/index/streamplayer.php
+++ inc/index/streamplayer.php
@@ -1,15 +1,9 @@
 <div class="mdl-grid mdl-grid--no-spacing fill">
     <div class="mdl-cell mdl-cell--9-col fill" id="videoBox">
         <div class="live-player">
-            <video class="video-js vjs-default-skin vjs-fill vjs-big-play-centered"
-                   data-setup='{"aspectRatio":"16:9","controls": true, "autoplay": true, "preload": "auto"}'
-                   id="streamPlayer"
-                   width="100%" height="100%"
-                   poster="//<?= $surl ?>/img/channel/channel_<?= $streamkey ?>.png"
-            >
-                <source src="rtmp://<?= $surl ?>/live/<?= $streamkey ?>" type="rtmp/flv" label='Flash'/>
-                <source src="//<?= $surl ?>/hls/<?= $streamkey ?>.m3u8" type="application/x-mpegurl" label='HLS'/>
-            </video>
+			<video class="video-js vjs-default-skin vjs-fill vjs-big-play-centered" controls autoplay preload id="streamPlayer"
+				   width="100%" height="100%" poster="//<?= $surl ?>/img/channel/channel_<?= $streamkey ?>.png">
+			</video>
         </div>
     </div>
     <div class="mdl-cell mdl-cell--3-col mdl-card mdl-shadow--2dp employer-form" id="channelchat">
@@ -47,3 +41,4 @@
         </form>
     </div>
 </div>
+<script> var ischat = true;</script>
