ba30d0605b4839148ee3ef54778ee736fc973829
Joel Bethke <joel.bethke@gmail.com>
2016-12-28 22:43:31 -0600

    Clean up ugly whitespace

--- inc/index/streamplayer.php
+++ inc/index/streamplayer.php
@@ -7,12 +7,10 @@
 			</video>
 		</div>
 	</div>
-
 	<div class="mdl-card mdl-shadow--2dp employer-form chat-container" id="channelchat">
 		<div class="mdl-card__title">
 			<span class="mdl-color-text--cyan-500">Channel Chat</span>
 		</div>
-
 		<div class="mdl-card__supporting-text full-height">
 			<div class="fill">
 				<div id="output" class="fill">
@@ -20,32 +18,23 @@
 				</div>
 			</div>
 		</div>
-
 		<div class="chatBorder"></div>
-
 		<form action="" method="POST" class="form" id="chatMessage">
-
 			<div class="chatinput" style="position:relative;">
-
-
 				<div class="mdl-grid--no-spacing chatbox">
 					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-
                         <textarea class="mdl-textfield__input" type="text" name="inputMessage" id="inputMessage"
 								  rows="1" autocomplete="off"/></textarea>
-
 						<label class="mdl-textfield__label" for="inputMessage">Enter message</label>
 					</div>
 					<button type="submit" form="chatMessage"
 							class="mdl-button mdl-js-button mdl-button--icon sendbutton">
 						<i class="material-icons sendbuttonicon">send</i>
 					</button>
-
 				</div>
 			</div>
 
 		</form>
 	</div>
-
 </div>
 <script> var ischat = true;</script>
