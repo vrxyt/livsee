d23e3795f05651fd5a7da3694f94de8e74af47f0
Joel Bethke <joel.bethke@gmail.com>
2017-03-01 14:35:32 -0600

    Update to show stream channel title in web page title.

--- index.php
+++ index.php
@@ -104,7 +104,10 @@ if ($page === 'download') {
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?= $sitetitle ?> Site</title>
+	<title><?= $sitetitle ?> Site<?php if (!empty($streamkey)) {
+			echo ' - Watching ' . $user->updateStreamkey($streamkey, 'channel');
+		} ?>
+	</title>
 	<link href="/img/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500,300,100,700,900' rel='stylesheet'
 		  type='text/css'>
