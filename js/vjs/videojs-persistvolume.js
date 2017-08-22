1e9ff7410ab1ea4f9cf9ecdef80dad8cb9d52651
Joel Bethke <joel.bethke@gmail.com>
2017-08-22 17:02:37 -0500

    Update to video.js 6.2.6

--- js/vjs/videojs-persistvolume.js
+++ js/vjs/videojs-persistvolume.js
@@ -11,7 +11,7 @@
         define(['./video'], function(vjs){ factory(window, document, vjs) });
         // checking that module is an object too because of umdjs/umd#35
     } else if (typeof exports === 'object' && typeof module === 'object') {
-        factory(window, document, require('videojs-5.14.1.js'));
+	    factory(window, document, require('6.2.6/video.min.js'));
     } else {
         factory(window, document, videojs);
     }
